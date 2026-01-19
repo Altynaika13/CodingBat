943ffc90a31e2c5d6368a8686e2ceecab45a123f
Altynai <uhah@MacBook-Pro-Uh.local>
2026-01-19 09:36:58 +0600

    My name

--- src/resetAndRevert/RevertDemo.java
+++ src/resetAndRevert/RevertDemo.java
@@ -3,6 +3,7 @@ package resetAndRevert;
 public class RevertDemo {
     public static void main(String[] args) {
         System.out.println("Demo 1");
+        System.out.println("My name is Altynai");
     }
 
 }
