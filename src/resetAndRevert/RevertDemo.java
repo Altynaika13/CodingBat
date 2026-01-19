c476ec0fe00e9bf070947cb41f58c8f85a080fe0
Altynai <uhah@MacBook-Pro-Uh.local>
2026-01-19 09:46:42 +0600

    Revert "My name"
    This reverts commit 943ffc90a31e2c5d6368a8686e2ceecab45a123f.

--- src/resetAndRevert/RevertDemo.java
+++ src/resetAndRevert/RevertDemo.java
@@ -3,7 +3,6 @@ package resetAndRevert;
 public class RevertDemo {
     public static void main(String[] args) {
         System.out.println("Demo 1");
-        System.out.println("My name is Altynai");
     }
 
 }
